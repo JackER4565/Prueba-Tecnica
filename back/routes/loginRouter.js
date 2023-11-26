5e3af1d17855ba042fa335e4c065f553d5d76e62
Fabrizio Rondinella <fabrondinella@gmail.com>
2023-11-25 22:49:35 -0300

    First Commit

--- back/routes/loginRouter.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const loginRouter = Router();
-
-const { loginController } = require("../controllers/loginController");
-
-//login
-loginRouter.post("/", loginController);
-
-module.exports = loginRouter;
